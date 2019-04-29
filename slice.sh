f18b5c9ec2fabe00643148b5a87eb0dc9a99dff5
easteregg <easteregg@verfriemelt.org>
2019-04-29 10:24:12 +0200

    +slicing tables too

--- slice.sh
+++ slice.sh
@@ -24,17 +24,24 @@ for FILE in $(find */* -name '*.sql' -type f -not -iwholename '*.svn*' ); do
             file = "head"; 
         } 
 
-        if ( match($0,/CREATE OR REPLACE FUNCTION ([a-zA-Z0-9_\.\s]+)/,a) ) { 
+        if ( match($0,/CREATE (OR REPLACE FUNCTION|TABLE) ([a-zA-Z0-9_\.\s]+)/,a) ) { 
 
-            # print $0;
-            file=a[1]; 
+            # print $0 $1;
+            file   = a[2]; 
             count += 10;
-        
+
+            # print count ": " a[2];
+
+            if ( match(a[1],/TABLE/ ) ) {
+              type = "table";
+            } else {
+              type = "function";
+            }
         }
 
-        # print count ": " a[1];
 
-        print $0 > sprintf("%04d-%s.sql",count,file); 
+
+        print $0 > sprintf("%04d-%s-%s.sql",count,type,file); 
 
     };' "$DIR/sources/$FILE"
 
